80a5357adf88ab83ed692db8f6d3359dc6dc0776
facepalmua <facepalmua@gmail.com>
2020-01-07 20:23:46 +0200

    contacts

--- app/js/common.js
+++ app/js/common.js
@@ -20,6 +20,11 @@ wow.init();
 $(document).ready(function() {
     console.log( "ready!" );
 
+    $("#select-file").change(function() {
+        filename = this.files[0].name;
+        $('.label-for-file').html('Выбраный файл:' + filename)
+    });
+
     $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
         disableOn: 700,
         type: 'iframe',
