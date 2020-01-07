fb5416614c551c1c4ffaa69516eea5ffe3cece56
facepalmua <facepalmua@gmail.com>
2020-01-07 13:42:46 +0200

    gallery page

--- app/js/common.js
+++ app/js/common.js
@@ -20,6 +20,29 @@ wow.init();
 $(document).ready(function() {
     console.log( "ready!" );
 
+    $('.zoom-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
+
     $('.dropdown').click(function () {
        $('.dropdown-menu').show()
     });
